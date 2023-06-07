f8fef5c44824efba4afedfedc5481072ef996196
Leonardo Andrés Crespo Sarango <lcrespos@est.ups.edu.ec>
2023-06-06 20:05:47 -0500

    PROYECTO

--- Usuario/Usuario/src/main/java/com/Usuario/Usuario/Service/CORSFilter.java
+++ Usuario/Usuario/src/main/java/com/Usuario/Usuario/Service/CORSFilter.java
@@ -6,7 +6,7 @@ import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
-
+//comentario random
 @Provider
 public class CORSFilter implements ContainerResponseFilter {
 
